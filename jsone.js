61ce0c554cd46648598a974a0b813843d8279a6d
= <=>
2016-08-07 08:48:54 -0700

    progress on ui, schema flushed out a bit, some comments

--- jsone.js
+++ jsone.js
@@ -1,107 +1,179 @@
-(function(root){
-	"use strict"
+(function(root) {
+	'use strict';
 
-	var jsone = function(config){
+	var jsone = function(config) {
 		var self = this;
 
 		self.__config = config || {};
-		self.__node = config.node || document.querySelector(config.node) || document.getElementById('jsone') || document.body;
+		// the node we write into. Take provided node, query selector, id=jsone or just the body
+		self.__node = DOM(config.node || document.querySelector(config.node) || document.getElementById('jsone') || document.body);
 
-		self.json = function(json){
-			if(!json){ self.initJSON(); return false; }
-			if(typeof json === 'object'){
+		// accept a javascript object or a URL to one
+		self.json = function(json) {
+			if (!json) {
+				self.initJSON(); return false;
+			}
+			if (typeof json === 'object') {
 				self.__json = json;
 				self.initJSON();
 			} else {
-				__mdd.prototype.http(json, function(json){
-					if(!json){ console.error('JSONE: Could not load json', json); }
+				__mdd.prototype.http(json, function(json) {
+					if (!json) {
+						console.error('JSONE: Could not load json', json);
+					}
 					self.json(json);
 				});
 			}
-		}
+		};
 
-		self.initJSON = function(){
-			self.loopJSON(self.__json, [], function(node, path, type){
+		// callback for json ready
+		self.initJSON = function() {
+			self.loopJSON(self.__json, [], function(node, path, type) {
 				self.renderNode(node, path, type);
 			});
 			console.log('json', self.__json, self.__config);
-		}
+		};
 
-		self.schema = function(schema){
-			if(!schema){ self.initSchema(); return false; }
-			if(typeof schema === 'object'){
+		// accept a javascript object for the scheme or a URL
+		self.schema = function(schema) {
+			if (!schema) {
+				self.initSchema(); return false;
+			}
+			if (typeof schema === 'object') {
 				self.__schema = schema;
 				self.initSchema();
 			} else {
-				__mdd.prototype.http(schema, function(schema){
-					if(!schema){ console.error('JSONE: Could not load schema', schema); }
+				__mdd.prototype.http(schema, function(schema) {
+					if (!schema) {
+						console.error('JSONE: Could not load schema', schema);
+					}
 					self.schema(schema);
 				});
 			}
-		}
-		self.initSchema = function(){
-			console.log('schema', self.__schema, self.__config);
-		}
+		};
+
+		//callback for when scemea is ready
+		self.initSchema = function() {
+			self.prepareSchema(self.__schema, []);
+		};
+
+		self.prepareSchema = function(node, path) {
+			for (var k in node) {
+
+			}
+		};
+
+		self.getRulesForPath = function(path) {};
 
 		self.json(self.__config.json || self.__config.json);
 		self.schema(self.__config.schema || self.__config.schema);
 
-		self.loopJSON = function(node, path, func){
+		// exec func on every part of the object, continue looping if the item is an object or array
+		self.loopJSON = function(node, path, func) {
 			var type = type = Array.isArray(node) ? 'array' : typeof node;
 
-			if(path.length){
+			if (path.length) {
 				func(node, path, type);
 			}
 
-			if(type === 'object' || type === 'array'){
-				for(var k in node){
+			if (type === 'object' || type === 'array') {
+				for (var k in node) {
 					self.loopJSON(node[k], path.concat(k), func);
 				}
 			}
-		}
-		self.renderNode = function(node, path, type){
-			var indent = path.length-1, joinpath = path.join('/'),
+		};
+
+		// render the ui for every segment of the object
+		self.renderNode = function(node, path, type) {
+			var indent = path.length - 1,
+				joinpath = path.join('/'),
 				css = {
-					'padding-left': (indent*20) + 'px',
+					'padding-left': (indent * 20) + 'px',
 					display: indent > 0 ? 'none' : 'block'
 				};
-			var row = mdd().new('section').html('path: '+joinpath).css(css).attr({'data-parent-path': path.slice(0, -1).join('/'), 'data-path': joinpath}).appendTo(self.__config.node).on('click', function(e){
-				if(type === 'object' || type === 'array'){
-					if(row.expanded){
-						self.__nodemdd.find('section[data-parent-path="'+joinpath+'"]').css({display: 'none'});
-						row.expanded = false;
-					} else {
-						self.__nodemdd.find('section[data-parent-path="'+joinpath+'"]').css({display: 'block'});
-						row.expanded = true;
+			var row = DOM().new('div').class('jsone-row')
+				.append(DOM().new('span'))
+				.append(DOM().new('span').text('path: ' + joinpath))
+				.css(css)
+				.attr({
+					'data-parent-path': path.slice(0, -1).join('/'),
+					'data-path': joinpath
+				})
+				.appendTo(self.__json_rows)
+				.on('click', function(e) {
+					if (type === 'object' || type === 'array') {
+						if (row.elements[0].getAttribute('data-expanded') === '1') {
+							self.__json_rows.find('div.jsone-row[data-parent-path^="' + joinpath + '"]').css({
+								display: 'none'
+							}).attr({
+								'data-expanded': '0'
+							});
+							row.attr({
+								'data-expanded': '0'
+							});
+						} else {
+							self.__json_rows.find('div.jsone-row[data-parent-path="' + joinpath + '"]').css({
+								display: 'block'
+							}).attr({
+								'data-expanded': '0'
+							});
+							row.attr({
+								'data-expanded': '1'
+							});
+						}
 					}
-				}
-			});
-		}
-		self.__node.classList.add('jsone');
-		self.__nodemdd = mdd(self.__node);
+					self.renderHelpPath(row, node, path, type);
+				});
+		};
+
+
+		self.renderHelpPath = function(row, node, path, type) {
+			self.__json_help.html('');
+			DOM().new('div').text('path: ' + path.join('/')).appendTo(self.__json_help);
+		};
+
+		self.__node.elements[0].classList.add('jsone');
+		self.__json_rows = DOM().new('div').class('jsone-rows').appendTo(self.__node);
+		self.__json_help = DOM().new('div').class('jsone-help').appendTo(self.__node);
 
 		__mdd.prototype.stylesheet({
-			'.jsone section:hover': {
-				'background-color': 'red'
+			'.jsone': {
+				'display': 'flex'
+			},
+			'.jsone-rows': {
+				'width': '50%',
+				'padding': '2px'
+			},
+			'.jsone-help': {
+				'background-color': '#eaeaea',
+				'width': '50%',
+				'padding': '2px'
+			},
+			'.jsone-row span': {
+				'display': 'inline-block',
+				'padding': '2px 0'
+			},
+			'.jsone-row:hover': {
+				'background-color': '#eaeaea'
 			}
-		}, 'json-sheet');
+		}, 'jsone-sheet');
 
 		return this;
 	};
 
 
 	/* dom utils == mini dandom == mdd */
-	if(!window.__mdd){
-		window.__mdd = function(sel, target){
+	if (!window.__mdd) {
+		window.__mdd = function(sel, target) {
 			this.__mdd = true;
-			if(typeof sel === 'object'){
+			if (typeof sel === 'object') {
 				this.elements = sel.__mdd ? sel : [sel];
-			} else if(sel){
-				this.elements = [].slice.call((target ? target.__mdd ? target.elements[0] : target : document).querySelectorAll(sel))
+			} else if (sel) {
+				this.elements = [].slice.call((target ? target.__mdd ? target.elements[0] : target : document).querySelectorAll(sel));
 			}
 			return this;
-		}
-		__mdd.prototype.on = function(eventNames, execFunc){
+		};
+		__mdd.prototype.on = function(eventNames, execFunc) {
 			var self = this;
 			eventNames.split(/[\s,]+/).forEach(function(eventName) {
 				if (eventName) {
@@ -111,59 +183,73 @@
 				}
 			});
 			return this;
-		}
-		__mdd.prototype.find = function(sel){
+		};
+		__mdd.prototype.find = function(sel) {
 			return new __mdd(sel, this);
-		}
-		__mdd.prototype.new = function(type){
+		};
+		__mdd.prototype.new = function(type) {
 			this.elements = [document.createElement(type)];
 			return this;
-		}
-		__mdd.prototype.css = function(css){
+		};
+		__mdd.prototype.css = function(css) {
 			this.elements.forEach(function(element) {
 				for (var K in css) {
 					element.style.setProperty(K, css[K] + '');
 				}
 			});
 			return this;
-		}
-		__mdd.prototype.attr = function(attr){
+		};
+		__mdd.prototype.class = function(className) {
+			this.elements.forEach(function(element) {
+				element.className = className;
+			});
+			return this;
+		};
+		__mdd.prototype.attr = function(attr) {
 			this.elements.forEach(function(element) {
 				for (var K in attr) {
 					element.setAttribute(K, attr[K]);
 				}
 			});
 			return this;
-		}
-		__mdd.prototype.appendTo = function(target){
+		};
+		__mdd.prototype.appendTo = function(target) {
 			var self = this;
 			var targ = target.elements || [target];
 			targ.forEach(function(targetElement) {
-				self.elements.forEach(function(element){
+				self.elements.forEach(function(element) {
 					targetElement.appendChild(element);
-				})
+				});
 			});
 			return this;
-		}
-		__mdd.prototype.text = function(text){
-			this.elements.forEach(function(element){
+		};
+		__mdd.prototype.append = function(el) {
+			this.elements.forEach(function(element) {
+				el.elements.forEach(function(apel) {
+					element.appendChild(apel);
+				});
+			});
+			return this;
+		};
+		__mdd.prototype.text = function(text) {
+			this.elements.forEach(function(element) {
 				element.textContent = text;
 			});
 			return this;
-		}
-		__mdd.prototype.html = function(html){
-			this.elements.forEach(function(element){
+		};
+		__mdd.prototype.html = function(html) {
+			this.elements.forEach(function(element) {
 				element.innerHTML = html;
 			});
 			return this;
-		}
-		__mdd.prototype.stylesheet = function(sheet, id){
-			if( typeof sheet === 'object' ){
+		};
+		__mdd.prototype.stylesheet = function(sheet, id) {
+			if (typeof sheet === 'object') {
 				var stylesheet = '';
-				Object.keys(sheet).forEach(function(selector){
+				Object.keys(sheet).forEach(function(selector) {
 					stylesheet += selector + ' { ';
-					Object.keys(sheet[selector]).forEach(function(property){
-						stylesheet += property + ':' + sheet[selector][property];
+					Object.keys(sheet[selector]).forEach(function(property) {
+						stylesheet += property + ':' + sheet[selector][property] + ';';
 					});
 					stylesheet += ' } ';
 				});
@@ -179,21 +265,21 @@
 			}
 			head.appendChild(style);
 
-		}
-		__mdd.prototype.http = function(){
+		};
+		__mdd.prototype.http = function() {
 			var config = {}, url, callback, ret;
 			for (var i = arguments.length - 1; i >= 0; i--) {
-				if(typeof arguments[i] === 'object'){
+				if (typeof arguments[i] === 'object') {
 					config = arguments[i];
-				} else if(typeof arguments[i] === 'string'){
+				} else if (typeof arguments[i] === 'string') {
 					url = arguments[i];
-				} else if(typeof arguments[i] === 'function'){
+				} else if (typeof arguments[i] === 'function') {
 					callback = arguments[i];
 				}
 			}
 			config.type = config.type || config.data ? 'POST' : 'GET';
 			config.url = url || config.url || '';
-			config.callback = callback || config.callback || function(){};
+			config.callback = callback || config.callback || function() {};
 			config.data = config.data || '';
 
 			var request = new XMLHttpRequest();
@@ -207,7 +293,7 @@
 				if (config.json || config.url.substr(-5) === '.json') {
 					try {
 						ret = JSON.parse(request.responseText);
-					} catch (e) {
+					} catch ( e ) {
 						console.error(e);
 						ret = false;
 					}
@@ -221,9 +307,11 @@
 				config.callback(false);
 			};
 			request.send(config.data);
-		}
+		};
 	}
-	var mdd = function(sel, target){ return new __mdd(sel, target); }
+	var DOM = function(sel, target) {
+		return new __mdd(sel, target);
+	};
 
 	root.jsone = jsone;
 	return this;
